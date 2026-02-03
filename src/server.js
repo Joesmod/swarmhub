6298e22948df4acdf83bf498beef9ebf9849478e
Kah <kah@swarmhub.dev>
2026-02-03 01:13:11 +0000

    Add landing page with Queen Bee mascot 👑🐝

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const crypto = require('crypto');
 const db = require('./db');
@@ -7,6 +8,9 @@ const db = require('./db');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3847;
 
